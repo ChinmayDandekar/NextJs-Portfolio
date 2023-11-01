69c5cc043f7ecec2d579ada7b4fa0680f1ca4618
Chinmay Dandekar <chinmay2001.cd@gmail.com>
2023-11-01 19:41:04 +0530

    Transition Effect

--- src/components/TransitionEffect.jsx
+++ src/components/TransitionEffect.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 const TransitionEffect = () => {
   return (
       <>
-          {/* <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-primary'
+          <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-primary'
             initial={{x:"100%", width:"100%"}}
               animate={{ x: "0%", width: "0%" }}
               transition={{ duration: 0.5, ease: "easeInOut" }}
@@ -19,7 +19,7 @@ const TransitionEffect = () => {
             initial={{x:"100%", width:"100%"}}
               animate={{ x: "0%", width: "0%" }}
               transition={{delay:0.4, duration:0.5, ease:"easeInOut"}}
-          /> */}
+          />
       </>
   )
 }
