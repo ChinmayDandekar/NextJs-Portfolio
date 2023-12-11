ca46e143cd6dc48b41d235b3939524b8eaa67a6e
Chinmay Dandekar <chinmay2001.cd@gmail.com>
2023-12-11 14:39:29 +0530

    minor fixes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -64,7 +64,8 @@ const NavBar = () => {
           <CustomLink href='/' title="Home" className='mr-4' />
           <CustomLink href='/about' title="About" className='mx-4' />
           <CustomLink href='/projects' title="Projects" className='mx-4' />
-          <CustomLink href='/articles' title="Articles" className='mx-4' />
+          {/* <CustomLink href='/articles' title="Articles" className='mx-4' /> */}
+          <CustomLink href='/certificates' title="Certificates" className='ml-4' />
           {/* <CustomLink href='/contact' title="Contact" className='ml-4' /> */}
         </nav>
 
@@ -132,7 +133,8 @@ const NavBar = () => {
           <CustomMobileLink href='/' title="Home" className='' toggle={handleClick} />
           <CustomMobileLink href='/about' title="About" className='' toggle={handleClick} />
           <CustomMobileLink href='/projects' title="Projects" className='' toggle={handleClick} />
-          <CustomMobileLink href='/articles' title="Articles" className='' toggle={handleClick} />
+          {/* <CustomMobileLink href='/articles' title="Articles" className='' toggle={handleClick} /> */}
+          <CustomMobileLink href='/certificates' title="Certificates" className='' toggle={handleClick} />
         </nav>
 
         <nav className='flex items-center justify-center flex-wrap mt-2 '>
